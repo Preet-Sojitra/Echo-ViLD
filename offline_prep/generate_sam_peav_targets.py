dc84a9984db0678ead218e9a91485d387dd1276b
Preet Sojitra <preet.dev373@gmail.com>
2026-04-25 13:21:00 -0500

    add description in offline prep

--- offline_prep/generate_sam_peav_targets.py
+++ offline_prep/generate_sam_peav_targets.py
@@ -1,3 +1,10 @@
 """
-Loads M1's .pt files, runs SAM/PE-AV, saves target .pt files
+Loads M1's .pt files, runs SAM/PE-AV, saves target .pt files.
+
+Things to do:
+1. Take bboxes from M1 -> crop the image using bboxes
+ -> pass to peav -> will be our vanilla PEAV embeddings. Our baseline.
+2. Take bboxes -> crop -> apply SAM segmentation -> get mask -> blackout background -> pass to peav -> will be our sam_no_context embeddings.
+3. Take bboxes -> crop -> apply SAM segmentation -> get mask -> blackout background -> pass to peav -> all pass full image to peav -> average the embeddings -> sam_with_context_equal
+4. same as 3 but averaging with 80 20 ratio -> sam_with_context_80_20 embedding
 """
